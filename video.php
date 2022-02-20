5ea6db649dc0a4a4468b20805e5198abe8992b49
MikayP <sharpie075@gmail.com>
2022-02-19 19:46:50 -0600

    working saturday

--- video.php
+++ video.php
@@ -7,43 +7,53 @@
 
 <?php get_header();
 
-$video_file = get_field('video_file_url');
+$video_file_url_1 = get_field('video_file_url_1');
+$video_file_url_2 = get_field('video_file_url_2');
+$video_file_url_3 = get_field('video_file_url_3');
+
+$link_1 = get_field('link_1');
+$link_2 = get_field('link_2');
+$link_3 = get_field('link_3');
+
+$slider_text_1 = get_field('slider_text_1');
+$slider_text_2 = get_field('slider_text_2');
+$slider_text_3 = get_field('slider_text_3');
+
 $sub_header = get_field('subheading');
 
 ?>
 <div class="video-template">
 	<div class="video-container" >
     <div class="carousel" id="flickity" data-flickity='{ "draggable": false }'>
+   
     <div class="carousel-cell">
     	<video muted preload="metadata" poster="">
-			<source src="<?php echo $video_file; ?>" type="video/mp4">
+			<source src="<?php echo $video_file_url_1; ?>" type="video/mp4">
 			Your browser does not support the video tag.
         </video>
         <div class="background_layer_dark"> </div>  
         <div class="z-index">
-        <a class="center-txt mb-3 slider-header" href="#"><h1 >blah</h1></a>
+        <a class="mb-3 slider-header" href="<?php echo $link_1 ?>"><h1 ><?php echo $slider_text_1 ?> <i class="fas fa-arrow-right"></i></h1></a>
         </div>
-
-
     </div>
     <div class="carousel-cell">
     <video muted preload="metadata" poster="">
-			<source src="http://pflugarts.local/wp-content/uploads/2022/02/sample-5s.mp4" type="video/mp4">
+			<source src="<?php echo $video_file_url_2; ?>" type="video/mp4">
 			Your browser does not support the video tag.
         </video>
         <div class="background_layer_dark"> </div>  
         <div class="z-index">
-        <a class="center-txt mb-3 slider-header" href="#"> <h2 >second-content</h2></a>
+        <a class="mb-3 slider-header" href="<?php echo $link_2 ?>"> <h2 ><?php echo $slider_text_2 ?> <i class="fas fa-arrow-right"></i></h2> </a>
 </div>
     </div>  
     <div class="carousel-cell">
         <video muted preload="metadata" poster="">
-			<source src="/wp-content/uploads/2022/02/small.mp4" type="video/mp4">
+			<source src="<?php echo $video_file_url_3; ?>" type="video/mp4">
 			Your browser does not support the video tag.
         </video>
         <div class="background_layer_dark"> </div>  
         <div class="z-index">
-        <a class="center-txt mb-3 slider-header" href="#"><h2  >third-content</h2></a>
+        <a class="mb-3 slider-header" href="<?php echo $link_3 ?>"><h2  ><?php echo $slider_text_3 ?> <i class="fas fa-arrow-right"></i></h2></a>
         </div>
     </div>
 
